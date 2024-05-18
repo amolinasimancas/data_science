c9e48a1f3b51995f8b4abfd54157656161e029b2
amolinasimancas <antoniomolina282@gmail.com>
2024-05-18 18:07:19 -0300

    Update 18.05.2024

--- 21d_python/16/packing.py
+++ 21d_python/16/packing.py
@@ -1,26 +1,29 @@
 def get_packages_info(packages):
+
     weights = []
     countries = []
     answer = {
         "total_weight": 0,
         "destinations": {}
     }
+  
     for package in packages:
         weights.append(package[1])
         countries.append(package[2])
-    
+
     distinct_countries = list(set(countries))
     total_weight = round(sum(weights),2)
 
-    i = 0
-    countries_count = []
-    while i < len(distinct_countries):
-        for package in packages:
-            if distinct_countries[i] == package[2]:
-                countries_count[i] += 1
-        i += 1
+    destinations = {}
+    for arrival in distinct_countries:
+      destinations[arrival] = 0
+        
+    for package in packages:
+      _, _, arrival = package
+      destinations[arrival] += 1
     
     answer["total_weight"] = total_weight
+    answer["destinations"] = destinations
         
     return answer
 
