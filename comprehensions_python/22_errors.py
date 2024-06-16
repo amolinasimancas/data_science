7e428002649d986cd9079262e8ee98b37f4e4cd6
amolinasimancas <antoniomolina282@gmail.com>
2024-06-16 18:55:44 -0300

    Update 16.06.2024

---
+++ comprehensions_python/22_errors.py
@@ -0,0 +1,33 @@
+try:
+  print(0 / 0)
+except ZeroDivisionError as error:
+  print(error)
+
+try:
+  assert 1 != 1, "Uno no es igual que uno"
+except AssertionError as error:
+  print(error)
+
+try:
+  age = 10
+  if age < 18:
+    raise Exception('No se permiten menores de edad')
+except Exception as error:
+  print(error)
+
+print('Hola')
+
+try:
+  print(0 / 0)
+  assert 1 != 1, "Uno no es igual que uno"
+  age = 10
+  if age < 18:
+    raise Exception('No se permiten menores de edad')
+except ZeroDivisionError as error:
+  print(error)
+except AssertionError as error:
+  print(error)
+except Exception as error:
+  print(error)
+
+print('Hola 2')
