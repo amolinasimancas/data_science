94fa77b82826dd920355b4c7928ae09e2c283b7d
amolinasimancas <antoniomolina282@gmail.com>
2025-02-03 19:00:34 -0300

    Update 03.02.2025

---
+++ python_21d/11_dos_vocales.py
@@ -0,0 +1,6 @@
+def find_words_with_two_vowels(words):
+    print("Hello world!")
+
+find_words_with_two_vowels(["hello", "Python", "world", "platzi"])
+
+find_words_with_two_vowels(["text", "test", "python", "example"])
