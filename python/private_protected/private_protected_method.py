ed44f5304e976034993092a1b8666e554613b519
amolinasimancas <antoniomolina282@gmail.com>
2025-03-24 21:53:56 -0300

    Update 24.03.2025

---
+++ python/private_protected/private_protected_method.py
@@ -0,0 +1,26 @@
+class BaseClass:
+    def __init__(self):
+        self._protected_variable = 'Protected'
+        self.__private_variable = 'Private'
+
+    def _protected_method(self):
+        print('Este es un metodo protegido')
+
+    def __private_method(self):
+        print('Esto es un metodo privado')
+
+    def public_method(self):
+        self.__private_method()
+
+base = BaseClass()
+
+print(base._protected_variable)
+base._protected_method()
+
+base.public_method()
+
+'''
+Darán error por ser privados
+print(base.__private_variable)
+base.__private_method()
+'''
